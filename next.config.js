3cb1081ee9d9089fe017d99e1687c2ad2abbef73
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-13 14:13:29 -0300

    -actualizo versiones para poder hacer el build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  modularizeImports: {
+    '@mui/icons/material': {
+      transform: '@mui/icons-material/{{member}}'
+    }
+  }
 }
 
 module.exports = nextConfig
